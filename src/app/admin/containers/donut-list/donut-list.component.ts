edb13bae55feb96821dd27381cb28320eab8b2bf
Alex Maslk <amksandr@gmail.com>
2023-11-22 19:50:55 +0200

    ANB: Donut Card

--- src/app/admin/containers/donut-list/donut-list.component.ts
+++ src/app/admin/containers/donut-list/donut-list.component.ts
@@ -1,13 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { Donut } from '../../models/donut.model';
+import { DonutsMock } from '../../mock/donuts.mock';
 
 @Component({
   selector: 'app-donut-list',
   template: `
     <div>
-      <app-donut-card [donut]="donuts[0]"></app-donut-card>
-      <app-donut-card [donut]="donuts[1]"></app-donut-card>
-      <app-donut-card [donut]="donuts[2]"></app-donut-card>
+      <ng-container *ngIf="donuts.length; else nothing">
+        <app-donut-card 
+        *ngFor="let donut of donuts; trackBy: trackById; index as i"
+        [donut]="donut"></app-donut-card>
+      </ng-container>
+      <ng-template #nothing>
+        <p>No Donuts here...</p>
+      </ng-template>
     </div>
   `,
   styles: [
@@ -17,29 +23,10 @@ export class DonutListComponent implements OnInit {
   donuts!: Donut[];
 
   ngOnInit(): void {
-    this.donuts = [
-      {
-        id: 'y8z0As',
-        name: 'Just Chocolate',
-        icon: 'just-chocolate',
-        price: 119,
-        description: 'For the pure chocoholic.',
-      },
-      {
-        id: '3u98Kl',
-        name: 'Glazed Fudge',
-        icon: 'glazed-fudge',
-        price: 129,
-        promo: true,
-        description: 'Sticky perfection.',
-      },
-      {
-        id: 'ae098s',
-        name: 'Caramel Swirl',
-        icon: 'caramel-swirl',
-        price: 129,
-        description: 'Chocolate drizzled with caramel.',
-      },
-    ];
+    this.donuts = DonutsMock;
   }
+
+  trackById(index: number, value: Donut): string {
+    return value.id;
+  };
 }
--- src/app/pages/temp-essntl/temp-ref/temp-ref.component.ts
+++ src/app/pages/temp-essntl/temp-ref/temp-ref.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Donut } from 'src/app/admin/models/donut.model';
 
 @Component({
   selector: 'app-temp-ref',
@@ -16,6 +17,16 @@ import { Component, OnInit } from '@angular/core';
       ::
       <span>{{ messageInput1.value }}</span>
     </p>    
+
+    <ng-container *ngIf="donuts.length; then card; else nothing"></ng-container>
+
+      <ng-template #card>
+        <p>[donut]="donuts[0].name"</p>
+        <p>[donut]="donuts[1].name"</p>
+      </ng-template>
+      <ng-template #nothing>
+        <p>No Donuts here...</p>
+      </ng-template>
   `,
   styles: [`
     .container { display: flex }
@@ -25,9 +36,34 @@ export class TempRefComponent implements OnInit {
   message!: string;
   newMessage!: string;
   message1!: string;
+  donuts!: any[];
 
   ngOnInit(): void {
     this.message = 'Message';
+    this.donuts = [
+      {
+        id: 'y8z0As',
+        name: 'Just Chocolate',
+        icon: 'just-chocolate',
+        price: 119,
+        description: 'For the pure chocoholic.',
+      },
+      {
+        id: '3u98Kl',
+        name: 'Glazed Fudge',
+        icon: 'glazed-fudge',
+        price: 129,
+        promo: true,
+        description: 'Sticky perfection.',
+      },
+      {
+        id: 'ae098s',
+        name: 'Caramel Swirl',
+        icon: 'caramel-swirl',
+        price: 129,
+        description: 'Chocolate drizzled with caramel.',
+      },
+    ];
   }
 
   handleClick(event: MouseEvent) {
